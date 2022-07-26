03a0eeacca0219a15cd669997934ba14656676db
Brian Shaginaw <bshaginaw120@gmail.com>
2022-07-26 15:01:21 -0400

    Create piston bindings for opengl framebuffer

---
+++ src/lib.rs
@@ -0,0 +1,54 @@
+use gl::types::GLuint;
+use opengl_graphics::{GlGraphics, Texture};
+use piston_window::{Context, Viewport};
+
+pub struct FrameBuffer {
+    fbo_id: GLuint,
+    texture: Texture,
+}
+
+impl FrameBuffer {
+    pub fn new(texture: Texture) -> Self {
+        let fbo_id;
+        unsafe {
+            let mut fbos: [GLuint; 1] = [0];
+            gl::GenFramebuffers(1, fbos.as_mut_ptr());
+            fbo_id = fbos[0];
+            gl::BindFramebuffer(gl::FRAMEBUFFER, fbo_id);
+            gl::FramebufferTexture2D(
+                gl::FRAMEBUFFER,
+                gl::COLOR_ATTACHMENT0,
+                gl::TEXTURE_2D,
+                texture.get_id(),
+                0,
+            );
+        }
+        FrameBuffer { fbo_id, texture }
+    }
+
+    pub fn draw<F>(&self, viewport: Viewport, gl: &mut GlGraphics, f: F)
+    where F: FnOnce(Context, &mut GlGraphics) {
+        unsafe {
+            gl::BindFramebuffer(gl::FRAMEBUFFER, self.fbo_id);
+        }
+
+        gl.draw(viewport, f);
+
+        unsafe {
+            gl::BindFramebuffer(gl::FRAMEBUFFER, 0);
+        }
+    }
+
+    pub fn texture(&self) -> &Texture {
+        return &self.texture;
+    }
+}
+
+impl Drop for FrameBuffer {
+    fn drop(&mut self) {
+        unsafe {
+            gl::BindFramebuffer(gl::FRAMEBUFFER, 0);
+            gl::DeleteFramebuffers(1, &self.fbo_id as *const GLuint);
+        }
+    }
+}
